cbadeb21e90d1406dc467826de20dec7d2bfbf4d
J M Archer <archer884@zoho.com>
2023-09-21 15:02:26 -0600

    Stopgap fix

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::{
     process,
 };
 
-use audiotags::{AudioTagEdit, AudioTagWrite, FlacTag, Tag};
+// use audiotags::{AudioTagEdit, AudioTagWrite, FlacTag, Tag};
 use clap::Parser;
+use metaflac::{block::VorbisComment, Block, Tag};
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -17,6 +18,9 @@ enum Error {
 
     #[error(transparent)]
     Id3(#[from] audiotags::Error),
+
+    #[error(transparent)]
+    Vorbis(#[from] metaflac::Error),
 }
 
 #[derive(Debug, Parser)]
@@ -40,6 +44,11 @@ impl<T: AsRef<Path>> PathGroup<T> {
 
     fn flac(&self) -> &Path {
         self.base.as_ref()
+    }
+
+    // for testing
+    fn flac_with_name(&self, name: &str) -> PathBuf {
+        self.base.as_ref().with_file_name(name)
     }
 
     fn mp3(&self) -> &Path {
@@ -69,32 +78,33 @@ fn run(args: Args) -> Result<()> {
         // metadata and printing it.
 
         let path_group = PathGroup::new(path);
-        let tag = Tag::new().read_from_path(path_group.mp3())?;
+        let tag = audiotags::Tag::new().read_from_path(path_group.mp3())?;
 
         // When we have the metadata, apparently we also have a writer for said metadata, which we
         // can then use to spray and pray. Who knew?
-        
+
         // Update: That shit doesn't even kind of work. I'm trying to do it manually now.
 
-        let mut output = FlacTag::new();
+        let mut flac = Tag::read_from_path(path_group.flac())?;
+        let comment = flac.vorbis_comments_mut();
 
         if let Some(album) = tag.album() {
-            output.set_album(album);
+            comment.set_album(vec![album.title]);
         }
 
         if let Some(title) = tag.title() {
-            output.set_title(title);
+            comment.set_title(vec![title]);
         }
 
         if let Some(artist) = tag.artist() {
-            output.set_artist(artist);
+            comment.set_artist(vec![artist]);
         }
 
         if let Some(track) = path_group.track() {
-            output.set_track_number(track as u16);
+            comment.set_track(track as u32);
         }
 
-        output.write_to_path(path_group.flac().to_str().unwrap())?;
+        flac.write_to_path(path_group.flac())?;
     }
 
     Ok(())
